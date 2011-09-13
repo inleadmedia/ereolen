766a80d33d8996de5341dfb49925801cef79d8a4
vs <vs@inleadmedia.com>
2011-09-13 17:51:58 +0300

    case 12151 - Video module on home page.

---
+++ sites/all/modules/ebog_embed_youtube/ebog_embed_youtube.tpl.php
@@ -0,0 +1,17 @@
+<div id="ebog_embed_youtube">
+  <div class = "ebog_embed_video">
+    <iframe 
+      title="YouTube video player" 
+      class="youtube-player" 
+      type="text/html" 
+      width="640" 
+      height="390" 
+      src="<?php echo $conf['embed_url']; ?>" 
+      frameborder="0" 
+      allowFullScreen>
+    </iframe>
+  </div>
+  <div class = "ebog_embed_descr">
+    <?php echo $conf['descr']; ?>
+  </div>
+</div>
