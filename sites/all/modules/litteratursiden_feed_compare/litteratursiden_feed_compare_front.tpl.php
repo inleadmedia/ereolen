a80c3b3bbb0aa745be57b2e30a920b443d19b95f
vs <vs@inleadmedia.com>
2011-09-13 16:35:56 +0300

    case 12147 - Added feed url field into admin settings form. Fix for caching mechanism.

--- sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_front.tpl.php
+++ sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_front.tpl.php
@@ -1,12 +1,12 @@
 <div id="feed_and_compare_front">
   <div id="feed_and_compare_front_right" style="float:left;width:49%;border:1px dashed red;">
-    ISBN: <?php echo $objects[0]->record['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
-    title: <?php echo $objects[0]->title; ?><br>
-    author: <?php echo $objects[0]->creators_string; ?><br>
+    ISBN: <?php echo $items[0]['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
+    title: <?php echo $items[0]['title']; ?><br>
+    author: <?php echo $items[0]['creators_string']; ?><br>
   </div>
   <div id="feed_and_compare_front_right" style="float:right;width:49%;border:1px dashed red;">
-    ISBN: <?php echo $objects[1]->record['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
-    title: <?php echo $objects[1]->title; ?><br>
-    author: <?php echo $objects[1]->creators_string; ?><br>
+    ISBN: <?php echo $items[1]['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
+    title: <?php echo $items[1]['title']; ?><br>
+    author: <?php echo $items[1]['creators_string']; ?><br>
   </div>
 </div>
--- sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_page.tpl.php
+++ sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_page.tpl.php
@@ -1,7 +1,7 @@
-<?php foreach ($objects as $object) { ?>
+<?php foreach ($items as $item) { ?>
   <div id="feed_and_compare_item" style="border:1px dashed red;margin-bottom:10px;">
-    ISBN: <?php echo $object->record['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
-    title: <?php echo $object->title; ?><br>
-    author: <?php echo $object->creators_string; ?><br>
+    ISBN: <?php echo $item['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
+    title: <?php echo $item['title']; ?><br>
+    author: <?php echo $item['creators_string']; ?><br>
   </div>
 <?php } ?>
