eb956d50e3e4b8c365e087bc48ceaf10bf8863a7
vs <vs@inleadmedia.com>
2011-09-14 16:46:36 +0300

    case 12151 - Fix embedding video, changes in url parsing.

--- sites/all/modules/ebog_embed_youtube/ebog_embed_youtube.tpl.php
+++ sites/all/modules/ebog_embed_youtube/ebog_embed_youtube.tpl.php
@@ -1,17 +1,20 @@
-<div id="ebog_embed_youtube">
-  <div class = "ebog_embed_video">
-    <iframe 
-      title="YouTube video player" 
-      class="youtube-player" 
-      type="text/html" 
-      width="640" 
-      height="390" 
-      src="<?php echo $conf['embed_url']; ?>" 
-      frameborder="0" 
-      allowFullScreen>
-    </iframe>
+<div class="ebog_embed_youtube">
+  <div class="ebog_embed_video">
+    <object style="width:207px;height:144px;">
+      <param name="movie" value="http://www.youtube.com/v/<?php echo $conf['embed_code'];?>">
+      <param name="allowFullScreen" value="true">
+      <param name="allowScriptAccess" value="always">
+      <embed
+        src="http://www.youtube.com/v/<?php echo $conf['embed_code'];?>"
+        type="application/x-shockwave-flash"
+        allowfullscreen="true"
+        allowScriptAccess="always"
+        width="207"
+        height="144"
+      >
+    </object>
   </div>
-  <div class = "ebog_embed_descr">
+  <div class="ebog_embed_descr">
     <?php echo $conf['descr']; ?>
   </div>
 </div>
