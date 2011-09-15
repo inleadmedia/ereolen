3c65e6d05ba8d80b7d2cc626e0d13123cd703f12
vs <vs@inleadmedia.com>
2011-09-15 10:33:08 +0300

    case 12147 - Feed+compare module. Fixing, reviewing.

--- sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_front.tpl.php
+++ sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_front.tpl.php
@@ -1,12 +1,25 @@
-<div id="feed_and_compare_front">
-  <div id="feed_and_compare_front_right" style="float:left;width:49%;border:1px dashed red;">
-    ISBN: <?php echo $items[0]['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
-    title: <?php echo $items[0]['title']; ?><br>
-    author: <?php echo $items[0]['creators_string']; ?><br>
-  </div>
-  <div id="feed_and_compare_front_right" style="float:right;width:49%;border:1px dashed red;">
-    ISBN: <?php echo $items[1]['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
-    title: <?php echo $items[1]['title']; ?><br>
-    author: <?php echo $items[1]['creators_string']; ?><br>
-  </div>
+<div class="feed_and_compare_front">
+  <?php $i = 0; ?>
+  <?php foreach ($items as $item) { ?>
+    <div class="feed_and_compare_front_item" style="border:1px dashed red;width:48%;float:left;margin:0 10px 10px 0;">
+      <div class="feed_and_compare_front_item_image">
+          <b>image:</b> <?php echo $item['image_url']; ?><br>
+      </div>
+      <div class="feed_and_compare_front_item_descr">
+        <div class="feed_and_compare_item_isbn">
+          <b>ISBN:</b> <?php echo $item['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
+        </div>
+        <div class="feed_and_compare_front_item_title">
+          <b>title:</b> <?php echo $item['title']; ?><br>
+        </div>
+        <div class="feed_and_compare_front_item_author">
+          <b>author:</b> <?php echo $item['creators_string']; ?><br>
+        </div>
+        <div class="feed_and_compare_front_item_teaser">
+          <b>teaser:</b> <?php echo $item['abstract']; ?><br>
+        </div>
+      </div>
+    </div>
+    <?php if ($i++ == 2) break; ?>
+  <?php } ?>
 </div>
--- sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_page.tpl.php
+++ sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_page.tpl.php
@@ -1,7 +1,28 @@
+
+
+<?php fb($items,'items');?>
+
+
+<div class="feed_and_compare_page">
 <?php foreach ($items as $item) { ?>
-  <div id="feed_and_compare_item" style="border:1px dashed red;margin-bottom:10px;">
-    ISBN: <?php echo $item['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
-    title: <?php echo $item['title']; ?><br>
-    author: <?php echo $item['creators_string']; ?><br>
+  <div class="feed_and_compare_page_item" style="border:1px dashed red;width:100%;margin:0 0 10px 0;">
+    <div class="feed_and_compare_page_item_image">
+        <b>image:</b> <?php echo $item['image_url']; ?><br>
+    </div>
+    <div class="feed_and_compare_page_item_descr">
+      <div class="feed_and_compare_item_isbn">
+        <b>ISBN:</b> <?php echo $item['record']['dc:identifier']['dkdcplus:ISBN'][0]; ?><br>
+      </div>
+      <div class="feed_and_compare_page_item_title">
+        <b>title:</b> <?php echo $item['title']; ?><br>
+      </div>
+      <div class="feed_and_compare_page_item_author">
+        <b>author:</b> <?php echo $item['creators_string']; ?><br>
+      </div>
+      <div class="feed_and_compare_page_item_teaser">
+        <b>teaser:</b> <?php echo $item['abstract']; ?><br>
+      </div>
+    </div>
   </div>
 <?php } ?>
+</div>
