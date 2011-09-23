b7dfbbe7381e12853b2128e9dc9d8d48ab71701b
INLEAD <info@inlead.dk>
2011-09-23 11:47:24 +0000

    Made the site ready for a mobile version.

--- page.tpl.php
+++ page.tpl.php
@@ -8,6 +8,7 @@
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
