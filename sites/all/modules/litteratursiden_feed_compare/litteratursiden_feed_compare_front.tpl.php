a42502188e22357d07cb10f6128d5e80aa3a768c
AnatolyMuntean <am@inleadmedia.com>
2011-09-26 11:24:33 -0400

    case 12147 - Some code cleanup.

--- sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_front.tpl.php
+++ sites/all/modules/litteratursiden_feed_compare/litteratursiden_feed_compare_front.tpl.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @file
+ */
+?>
 <div class="feed_and_compare_front">
 
   <?php if ($items['status'] == 'empty') { ?>
@@ -17,20 +22,20 @@
       <?php if (is_numeric($key)) { ?>
         <div class="feed_and_compare_front_item" style="border:1px dashed red;width:48%;float:left;margin:0 10px 10px 0;">
           <div class="feed_and_compare_front_item_image">
-            <b>image:</b> <?php echo theme_image($item['image_url'], '', '', array('width'=>'100px'), FALSE); ?><br>
+            <b>image:</b> <?php echo theme_image($item['image_url'], '', '', array('width' => '100px'), FALSE); ?><br />
           </div>
           <div class="feed_and_compare_front_item_descr">
             <div class="feed_and_compare_item_isbn">
-              <b>ISBN:</b> <?php echo $item['isbn']; ?><br>
+              <b>ISBN:</b> <?php echo $item['isbn']; ?><br />
             </div>
             <div class="feed_and_compare_front_item_title">
-              <b>title:</b> <?php echo $item['title']; ?><br>
+              <b>title:</b> <?php echo $item['title']; ?><br />
             </div>
             <div class="feed_and_compare_front_item_author">
-              <b>author:</b> <?php echo $item['author']; ?><br>
+              <b>author:</b> <?php echo $item['author']; ?><br />
             </div>
             <div class="feed_and_compare_front_item_description">
-              <b>teaser:</b> <?php echo $item['description']; ?><br>
+              <b>teaser:</b> <?php echo $item['description']; ?><br />
             </div>
           </div>
         </div>
