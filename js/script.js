8f5763a4fbcba8c828b4eb84bcb1a5717b71495b
Martin Cording <mc@inleadmedia.com>
2011-10-07 12:46:13 +0200

    Added autoload of library in dropdown.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 jQuery(function($){
-	 $('.view-faq .item-list').each(function(){
-		 link = $(this).find('div.views-field-tid');
-		 $(this).find('div.views-field-tid').remove();
-		 $(this).append(link[0]);
-	 });
+
+   $('.view-faq .item-list').each(function(){
+     link = $(this).find('div.views-field-tid');
+     $(this).find('div.views-field-tid').remove();
+     $(this).append(link[0]);
+  });
+
+  $('#user-login #edit-name').blur(function(){
+      jQuery.getJSON( '/getlibrary/'+$(this).val(),function(data){
+        if(data.elib_library){
+          $('#user-login #edit-library option[value='+data.elib_library+']').attr('selected','selected');
+        }
+      });
+   });
+
 });
