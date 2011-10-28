6ca49f8d1f3cc26e352c6b693fa4a95696a90c6f
AnatolyMuntean <am@inlead.dk>
2011-10-28 16:40:22 +0300

    case 12600 - Removed last checkbox from popup.

--- sites/all/modules/ebog_download/templates/ebog_download-checkboxes.tpl.php
+++ sites/all/modules/ebog_download/templates/ebog_download-checkboxes.tpl.php
@@ -1,5 +1,4 @@
 <div class="ebog-download-confirm">
   <p><input type="checkbox" name="rule_1" id="rule_1" /><label for="rule_1"><?php print t('Ja, jeg har et Adobe ID.'); ?></label></p>
   <p><input type="checkbox" name="rule_2" id="rule_2" /><label for="rule_2"><?php print t('Ja, jeg har installeret @reader.', array('@reader' => $data['reader'])); ?></label></p>
-  <p><input type="checkbox" name="rule_3" id="rule_3" /><label for="rule_3"><?php print t('Ja, jeg indforstaet med at jeg skal download til men computer.'); ?></label></p>
 </div>
