7561f052f51c6361a7899bc719761bfd69343cf4
AnatolyMuntean <am@inlead.dk>
2011-10-31 18:15:24 +0200

    case 12600 - Template fixes.

--- sites/all/modules/ebog_download/templates/ebog_download-info.tpl.php
+++ sites/all/modules/ebog_download/templates/ebog_download-info.tpl.php
@@ -8,6 +8,6 @@
 <div class="ebog-download-info">
   <p><?php echo $data['message']; ?></p>
   <?php if (isset($data['link'])) { ?>
-  <p><a class="down_link" href="<?php echo $data['link']; ?>"><?php print t('Download');?></a></p>
+  <p><a class="ebog-dlink" class="down_link" href="<?php echo $data['link']; ?>"><?php print t('Download');?></a></p>
   <?php } ?>
 </div>
