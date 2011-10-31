d74f46e88a3ee7926b0967cc9fdb4b9a27b6b677
INLEAD <info@inlead.dk>
2011-10-31 14:37:05 +0100

    Added links to the labels.

--- sites/all/modules/ebog_download/templates/ebog_download-checkboxes.tpl.php
+++ sites/all/modules/ebog_download/templates/ebog_download-checkboxes.tpl.php
@@ -1,4 +1,4 @@
 <div class="ebog-download-confirm">
-  <p><input type="checkbox" name="rule_1" id="rule_1" /><label for="rule_1"><?php print t('Ja, jeg har et Adobe ID.'); ?></label></p>
-  <p><input type="checkbox" name="rule_2" id="rule_2" /><label for="rule_2"><?php print t('Ja, jeg har installeret @reader.', array('@reader' => $data['reader'])); ?></label></p>
+  <p><input type="checkbox" name="rule_1" id="rule_1" /><label for="rule_1"><?php print t('Ja, jeg har et Adobe ID') . ' (<a href="adobe">l&aelig;s mere</a>).' ; ?></label></p>
+  <p><input type="checkbox" name="rule_2" id="rule_2" /><label for="rule_2"><?php print t('Ja, jeg har installeret @reader', array('@reader' => $data['reader'])) . ' (<a href="software">l&aelig;s mere</a>).' ; ?></label></p>
 </div>
