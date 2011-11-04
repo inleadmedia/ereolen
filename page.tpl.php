651cf809297f29380bf50090ea1854a4b165e603
INLEAD <info@inlead.dk>
2011-11-04 13:39:54 +0100

    Fixed the view of the status message.

--- page.tpl.php
+++ page.tpl.php
@@ -59,10 +59,7 @@
     <div id="main-content-container" class="container-<?php print $content_container_width; ?> clearfix">
       <div id="main-wrapper" class="column <?php print $main_content_classes; ?>">
         <?php if($help || $messages): ?>
-        <div class="statusBox clearfix">
-          <h3>Status</h3>
           <?php print $help; ?><?php print $messages; ?>
-        </div><!-- /.container-xx -->
         <?php endif; ?>
 
         <?php if (!empty($mission)) {
