d057b42ba3c3a9a3424e0ca80a12bc48c528a3db
Martin Cording <mc@inleadmedia.com>
2011-12-14 15:54:19 +0100

    Modified the VoxB look a bit more.

--- sites/all/modules/voxb/templates/ding_voxb-review-record.tpl.php
+++ sites/all/modules/voxb/templates/ding_voxb-review-record.tpl.php
@@ -7,6 +7,6 @@
 ?>
 
 <div class="voxbReview">
-  <?php print t('Written by'); ?> <em><?php echo htmlspecialchars($data['author']); ?></em>
-  <div class="reviewContent"><?php echo htmlspecialchars($data['review']); ?></div>
+  <?php print t('Written by'); ?> <strong><?php echo htmlspecialchars($data['author']); ?></strong>
+  <div class="reviewContent"><em><?php echo htmlspecialchars($data['review']); ?></em></div>
 </div>
--- sites/all/modules/voxb/templates/ding_voxb-tags.tpl.php
+++ sites/all/modules/voxb/templates/ding_voxb-tags.tpl.php
@@ -14,12 +14,8 @@ if(($data['voxb_item']->getTags()->getCount() < 0) || ($user->uid != 0 && $data[
       <div class="review-title"><?php echo t('Tags'); ?></div>
       <div class="recordTagHighlight">
       <?php
-        if($data['voxb_item']->getTags()->getCount() < 0) {
-          foreach ($data['voxb_item']->getTags() as $v) {
-            echo theme('voxb_tag_record', array('tag_name' => $v->getName()));
-          }
-        } else {
-          print t('There is currently no tags. Be the first to add one!');
+        foreach ($data['voxb_item']->getTags() as $v) {
+          echo theme('voxb_tag_record', array('tag_name' => $v->getName()));
         }
       ?>
       </div>
