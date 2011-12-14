3e5629e3a41fe05fa0e8afc5bd7209988f08d656
AnatolyMuntean <am@inlead.dk>
2011-12-14 19:47:02 +0200

    case 13202 - Re-download and re-loan procedure improvements.

--- sites/all/modules/ebog_download/js/ebog_download_scripts.js
+++ sites/all/modules/ebog_download/js/ebog_download_scripts.js
@@ -20,6 +20,36 @@
       clicked.hide();
       clicked.parent().append('<div class="ajax-loader"></div>');
 
+      if (clicked.hasClass('re-loan')) {
+        $('#ting-download-popup').remove();
+        $('<div id="ting-download-popup" title="' + Drupal.t('Confirm reloan') + '">' + Drupal.t('Are you sure you want to reloan this item?') + '</div>').dialog({
+          modal : true,
+          width: 'auto',
+          height: 'auto',
+          buttons: {
+            "OK" : function() {
+              button = $('#ting-download-popup').parents('.ui-dialog:first').find('button');
+              button.css('visibility', 'hidden');
+              button.parent().append('<div class="ajax-loader"></div>');
+              process_loan();
+            },
+            "Cancel" : function() {
+              $('#ting-download-popup').dialog('close');
+              clicked.parent().find('.ajax-loader').remove();
+              clicked.show();
+            }
+          }
+        });
+      }
+      else {
+        process_loan();
+      }
+
+      return false;
+    });
+
+    // Process loan/reloan/download
+    var process_loan = function() {
       $.ajax({
         type : 'post',
         url : href + '/popup',
@@ -28,7 +58,7 @@
           $('#ting-download-popup').remove();
           clicked.parent().find('.ajax-loader').remove();
           clicked.show();
-          
+
           if (response.status == false) {
 
             $('<div id="ting-download-popup" title="' + response.title + '">' + response.content + '</div>').dialog({
@@ -44,7 +74,7 @@
 
             return;
           }
-          
+
           $('<div id="ting-download-popup" title="' + response.title + '">' + response.content + '</div>').dialog({
             modal : true,
             width: 'auto',
@@ -60,9 +90,7 @@
           });
         }
       });
-
-      return false;
-    });
+    }
 
     // Check those checkboxes
     var check_rules = function() {
@@ -71,6 +99,7 @@
         $.ajax({
           type : 'post',
           url : href + '/request',
+          data : {'dload_only' : clicked.hasClass('re-download') ? 1 : 0},
           dataType : 'json',
           success : function(response) {
             button.css('visibility', 'visible');
