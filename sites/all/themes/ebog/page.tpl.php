031c7bcfe7347dfd12bb9952eaa6c0539b219f24
AnatolyMuntean <am@inlead.dk>
2012-05-21 14:47:32 +0400

    case 347 - User-scalable option enabled.

--- sites/all/themes/ebog/page.tpl.php
+++ sites/all/themes/ebog/page.tpl.php
@@ -8,7 +8,7 @@
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+  <meta name="viewport" content="width=device-width, user-scalable=1, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <link rel="apple-touch-icon" href="images/ereolen.png" />
