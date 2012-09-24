cc7d697b9da5560b21ae4900266b538ff1585f39
Jesper Kristensen <cableman@linuxdev.dk>
2012-09-24 14:26:08 +0200

    Fixed navigation tabs for the search carousel

--- sites/all/modules/ting_search_carousel/templates/ting_search_carousel.tpl.php
+++ sites/all/modules/ting_search_carousel/templates/ting_search_carousel.tpl.php
@@ -4,20 +4,15 @@
  *
  *
  * * Available variables:
- * - $tab_position: String with settings info, values: top,bottom,left,right.
  * - $searches: Array with each tab search.
  *
  */
 ?>
 
-<!-- The wrapper div is important because rs-carousel replaces it -->
-<div class="rs-carousel-wrapper <?php echo $toggle_description; ?>">
+<div class="rs-carousel-wrapper rs-carousel-compact">
   <div class="rs-carousel">
     <div class="rs-carousel-inner">
       <div class="ajax-loader"></div>
-      <?php if ($toggle_description): ?>
-        <div class="rs-carousel-title"></div>
-      <?php endif; ?>
       <ul class="rs-carousel-runner">
       </ul>
     </div>
