06160dde57c890f70d412ef76cc74c5faeb7dbb7
Jesper Kristensen <cableman@linuxdev.dk>
2012-09-27 15:29:29 +0200

    Added headline to publisher description

--- sites/all/themes/ebog/ting_object.tpl.php
+++ sites/all/themes/ebog/ting_object.tpl.php
@@ -72,13 +72,16 @@ if (module_exists('ding_voxb')) {
       <div class="facebook-like">
         <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fereolen.dk%2Fting%2Fobject%2F150028%3A<?php echo $elib_isbn; ?>&amp;send=false&amp;layout=box_count&amp;width=130&amp;show_faces=false&amp;action=recommend&amp;colorscheme=light&amp;font&amp;height=75" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:130px; height:75px;" allowTransparency="true"></iframe>
       </div>
-      <?php if (isset($object->abstract)) { ?>
+      <?php if (isset($object->abstract) && $object->abstract) { ?>
         <div class="abstract">
           <p><?php print $object->abstract; ?></p>
         </div>
       <?php } ?>
       <?php if (isset($object->publisherDescription)) { ?>
         <div class="publisherDescription">
+          <?php if (isset($object->abstract) && $object->abstract) { ?>
+            <h2><?php echo t('Publisher description'); ?></h2>
+          <?php } ?>
           <?php echo $object->publisherDescription; ?>
         </div>
       <?php } ?>
