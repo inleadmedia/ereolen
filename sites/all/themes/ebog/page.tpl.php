fe9ec38f610c7149235518f5ed2ada3ed91d587a
Rasmus Frey <rasmus@artz.dk>
2012-10-09 11:33:13 +0200

    added media query so defgo is not display on small screens

--- sites/all/themes/ebog/page.tpl.php
+++ sites/all/themes/ebog/page.tpl.php
@@ -117,11 +117,20 @@ maximum-scale=3.0" />
   <?php print $closure; ?>
   <!-- defgo.net script begin -->
   <script language="JavaScript">
-  <!--
   var popunder=false;
   var surveyOk=false;
   var loaded=false;
-  document.write('<SCR'+'IPT language=\"JavaScript\" src=\"https://www.defgo.net/popup_ium/upload/popup1111745468_1.asp?&t=1&s=1\"></SCR'+'IPT>');
+  
+  var query = "screen and (min-width:650px)";
+  enquire.register(query, {
+    match : function() {
+      <!--
+      document.write('<SCR'+'IPT language=\"JavaScript\" src=\"https://www.defgo.net/popup_ium/upload/popup1111745468_1.asp?&t=1&s=1\"></SCR'+'IPT>');
+      timeoutCall(0);
+      //-->
+    }
+  }, true).listen();
+
   function timeoutCall(time){
     if(loaded && surveyOk){
       PopUpWindow();
@@ -134,8 +143,6 @@ maximum-scale=3.0" />
         setTimeout("timeoutCall("+(time+500)+")",500);
     }
   }
-  timeoutCall(0);
-  //-->
   </script>
   <div style="display:none" id="defgonetmessage" class="defgoDivTag"></div><img style="display: none" border="0" src="http://app01.defgosoftware.net/defgo/blank.gif" width="1" height="1" onLoad="surveyOk=true">
   <!-- defgo.net script end -->
