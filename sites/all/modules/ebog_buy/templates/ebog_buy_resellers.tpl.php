b243d604e1b67e2eb13cc1722223e9270fb3ace6
Anatoly Muntean <am@inlead.dk>
2013-03-07 12:45:01 +0200

    case 2239 - Resellers list functionality.

---
+++ sites/all/modules/ebog_buy/templates/ebog_buy_resellers.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Resellers markup wrapper.
+ */
+?>
+<div class="item-resellers">
+  <p class="note"><?php print t('Please, select from the resellers below.'); ?></p>
+  <?php print $data['resellers']; ?>
+</div>
