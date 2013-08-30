b8646f29a6b95a35bef89f53e516bd79adb9d021
Jesper Kristensen <cableman@linuxdev.dk>
2013-08-30 14:46:08 +0200

    Changed preview buttons to use streaming service

---
+++ sites/all/modules/ebog_stream/templates/ebog-stream-iframe-stream-preview.tpl.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="da" lang="da" dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta content="no-cache, no-store" http-equiv="cache-control" />
+    <meta content="no-cache" http-equiv="pragma" />
+    <meta content="-1" http-equiv="expires" />
+    <link rel="stylesheet" type="text/css" href="https://reader.pubhub.dk/scripts/reader-1.3.3/css">
+    <script type="text/javascript" src="https://reader.pubhub.dk/scripts/reader-1.3.3/js"></script>
+    <script type="text/javascript">
+      var reader;
+        function InitReader() {
+          reader = new Reader ({
+            elementId: 'reader-container',
+            isbn : '<?php print $vars['isbn']; ?>',
+            notesEnabled: false
+          });
+          reader.Display();
+      }
+    </script>
+    <title>eReolen Stream - <?php print $vars['title'] ?></title>
+  </head>
+  <body onload="javascript:InitReader();">
+    <div id="reader-container"></div>
+  </body>
+</html>
