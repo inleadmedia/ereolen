93221555e4e4e29d983e3760b4667ee66bf3523a
Andrei Bunulu <bunulu@gmail.com>
2013-10-03 17:59:48 +0300

    case-3406 - Collapsible items

--- sites/all/themes/ebog/js/script.js
+++ sites/all/themes/ebog/js/script.js
@@ -1,6 +1,54 @@
 jQuery(function($){
   $(document).ready(function() {
 
+    // Collapsible blocks
+    var container = [
+      '.pane-latest-news .pane-content',
+      '.block-publizon_user .content',
+      '.block-ebog_author_portrait .content',
+      '.pane-embedvideo .pane-content',
+      '.pane-front .pane-content'
+    ];
+
+    // Collapsible blocks
+    function collapsibleBlocks() {
+      for (var i = container.length - 1; i >= 0; i--) {
+        if ($(container[i]).length !== 0) {
+          var block = $(container[i]);
+
+          // Make toggle feauture only for mobile screen
+          if ( $(document).width() < 540 - (window.innerWidth - $(document).width()) ) {
+            if (block.siblings('.collapsible-title').hasClass('active')) {
+              block.show();
+            } else {
+              block.hide();
+            }
+            block.addClass('collapsible');
+            block.siblings('h2').addClass('collapsible-title');
+          } else {
+            block.show().removeClass('collapsible');
+            block.siblings('h2').removeClass('collapsible-title');
+          }
+        }
+      }
+    }
+
+    // Init collapsible blocks
+    collapsibleBlocks();
+
+    // Check for changes on resize
+    $(window).resize(function() {
+      console.log(window.innerWidth - $(document).width());
+      collapsibleBlocks();
+    });
+
+    // Toggle block state on click
+    $('.collapsible-title').click(function () {
+      var title = $(this);
+      title.siblings('.collapsible').toggle();
+      title.toggleClass('active');
+    });
+
     // Make publisher description folderable.
     $('#ting-object .publisherDescription').expander({
       slicePoint:       350,
@@ -18,6 +66,5 @@ jQuery(function($){
     $('.publizon-loans-list li:last-child').addClass('last');
     $('.display-book').parent().find('.display-book:last').addClass('last');
 
-
-  })
+  });
 });
