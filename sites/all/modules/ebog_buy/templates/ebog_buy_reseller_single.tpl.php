27310697e60994c2e5a7b460aee31aa53deb32f0
Anatoly Muntean <am@inlead.dk>
2013-10-13 11:44:46 +0000

    case 3417 - Hardcoded currency.

--- sites/all/modules/ebog_buy/templates/ebog_buy_reseller_single.tpl.php
+++ sites/all/modules/ebog_buy/templates/ebog_buy_reseller_single.tpl.php
@@ -12,6 +12,6 @@
     <img height="50" src="<?php print $data['seller']->Logo; ?>" alt="<?php print $data['seller']->Name; ?>" title="<?php print $data['seller']->Name; ?>" />
   </p>
   <?php endif; ?>
-  <p class="price"><?php print t('Price: @price', array('@price' => $data['price'])); ?></p>
+  <p class="price"><?php print t('Price: @price DKK', array('@price' => $data['price'])); ?></p>
   <p class="buy"><a target="_blank" href="<?php print $data['url']; ?>"><button><?php print t('Buy'); ?></button></a></p>
 </div>
