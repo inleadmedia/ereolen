6b877fbab2bafcadf4a181363b4a0bd09ab7713c
Andrei Bunulu <bunulu@gmail.com>
2013-12-24 10:39:43 +0000

    case-3589 - Menu iphone issues

--- sites/all/modules/contrib/nice_menus/nice_menus.js
+++ sites/all/modules/contrib/nice_menus/nice_menus.js
@@ -6,20 +6,22 @@
 // Add Superfish to all Nice menus with some basic options.
 (function ($) {
   $(document).ready(function() {
-    $('ul.nice-menu').superfish({
-      // Apply a generic hover class.
-      hoverClass: 'over',
-      // Disable generation of arrow mark-up.
-      autoArrows: false,
-      // Disable drop shadows.
-      dropShadows: false,
-      // Mouse delay.
-      delay: Drupal.settings.nice_menus_options.delay,
-      // Animation speed.
-      speed: Drupal.settings.nice_menus_options.speed
-    // Add in Brandon Aaron’s bgIframe plugin for IE select issues.
-    // http://plugins.jquery.com/node/46/release
-    }).find('ul').bgIframe({opacity:false});
-    $('ul.nice-menu ul').css('display', 'none');
+    if ($(document).width() >= 540 - (window.innerWidth - $(document).width())) {
+      $('ul.nice-menu').superfish({
+        // Apply a generic hover class.
+        hoverClass: 'over',
+        // Disable generation of arrow mark-up.
+        autoArrows: false,
+        // Disable drop shadows.
+        dropShadows: false,
+        // Mouse delay.
+        delay: Drupal.settings.nice_menus_options.delay,
+        // Animation speed.
+        speed: Drupal.settings.nice_menus_options.speed
+      // Add in Brandon Aaron’s bgIframe plugin for IE select issues.
+      // http://plugins.jquery.com/node/46/release
+      }).find('ul').bgIframe({opacity:false});
+      $('ul.nice-menu ul').css('display', 'none');
+    }
   });
 })(jQuery);
